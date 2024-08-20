268f69f9ae8132f9a2d65402d2c4289e31d1683b
Piotr Kaczmarski <piotr.kaczmarski.ext@assetmax.ch>
2024-08-20 10:47:20 +0200

    Ch 2 - 2

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 import pygame
+from constants import *
 
 def main():
-    print("Starting asteroids!")
+    (succs, fails) = pygame.init()
+
+    print(f"{succs}, {fails}")
+
+    window = pygame.display.set_mode(
+            size=(SCREEN_WIDTH, SCREEN_HEIGHT)
+    )
+
+    while(True):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        window.fill(pygame.Color("black"))
+        pygame.display.flip()
+
+    pygame.quit()
+
+
 
 if __name__ == "__main__":
     main()
