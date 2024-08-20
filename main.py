0e95cea40898e6b45e7572851f98c75ef16f3468
Piotr Kaczmarski <piotr.kaczmarski.ext@assetmax.ch>
2024-08-20 10:50:25 +0200

    Ch 2 - 3

--- main.py
+++ main.py
@@ -10,12 +10,17 @@ def main():
             size=(SCREEN_WIDTH, SCREEN_HEIGHT)
     )
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         window.fill(pygame.Color("black"))
         pygame.display.flip()
+
+        dt = clock.tick(60)
 
     pygame.quit()
 
